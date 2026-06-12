88d3511a0bd2cd056cd579c7ad1013af0426eb31
vovchanskuy2 <68811379+vovchanskuy2@users.noreply.github.com>
2026-06-12 21:21:28 +0300

    add req
    add req

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# main.py
 import cv2
 import time
 import mediapipe as mp
@@ -5,11 +6,15 @@ import mediapipe as mp
 from detector import create_detectors, download_models
 from capture import get_capture
 from processor import process_frame
+from udp_sender import UDPSender
 
 
 def main():
+    download_models()
     cap = get_capture(0)
     pose_det, hand_det, face_det = create_detectors()
+
+    sender = UDPSender()
 
     while True:
         ret, frame = cap.read()
@@ -21,38 +26,32 @@ def main():
         rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb)
 
-        timestamp = int(time.time() * 1000)
+        t = time.time()
+        timestamp = int(t * 1000)
 
         pose_res = pose_det.detect_for_video(mp_image, timestamp)
-
-        hand_res = None
-        face_res = None
-
-        if pose_res.pose_landmarks:
-            pose_lm = pose_res.pose_landmarks[0]
-
-            # руки если видны запястья
-            if pose_lm[15].visibility > 0.4 or pose_lm[16].visibility > 0.4:
-                hand_res = hand_det.detect_for_video(mp_image, timestamp)
-
-            # лицо если виден нос
-            if pose_lm[0].visibility > 0.4:
-                face_res = face_det.detect_for_video(mp_image, timestamp)
+        hand_res = hand_det.detect_for_video(mp_image, timestamp)
+        face_res = face_det.detect_for_video(mp_image, timestamp)
 
         data = process_frame(
             pose_res.pose_landmarks if pose_res else None,
             hand_res.hand_landmarks if hand_res else None,
-            face_res.face_landmarks if face_res else None
+            hand_res.handedness if hand_res else None,
+            face_res.face_landmarks if face_res else None,
+            face_res.face_blendshapes if face_res else None,
+            face_res.facial_transformation_matrixes if face_res else None
         )
 
-        # DEBUG вывод
-        print("POSE:", None if data["pose"] is None else data["pose"].shape)
+        data["t"] = t
+
+        sender.send(data)
 
         cv2.imshow("frame", frame)
         if cv2.waitKey(1) & 0xFF == 27:
             break
 
     cap.release()
+    sender.close()
     cv2.destroyAllWindows()
 
 
